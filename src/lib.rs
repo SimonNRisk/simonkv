d09e050a0a1b6fe6909925fe90131ed3bf940deb
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2026-07-08 23:43:37 -0400

    ref: return option<&str> to fix sentinel value bug

--- src/lib.rs
+++ src/lib.rs
@@ -11,11 +11,8 @@ impl KVStore {
     pub fn set(&mut self, key: String, value: String) {
         self.map.insert(key, value);
     }
-    pub fn get(&self, key:String) -> String {
-        match self.map.get(&key) {
-            Some(val) => return val.clone(),
-            None => return String::from("")
-        }
+    pub fn get(&self, key:&str) -> Option<&str> {
+        self.map.get(key).map(|v| v.as_str())
     }
     pub fn delete(&mut self, key:String) {
         self.map.remove(&key);
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use simonkv::KVStore;
 fn main() {
     let mut kvstore = KVStore::new();
     kvstore.set(String::from("Test"), String::from("1"));
-    let fetched_val = kvstore.get(String::from("Test"));
-    println!("{fetched_val}");
+    let fetched_val = kvstore.get("Test");
+    match fetched_val {
+        Some(val) => println!("Got {val}"),
+        None => println!("No value")
+    }
+    let fetched_fake_val = kvstore.get("Fake");
+    match fetched_fake_val {
+        Some(val) => println!("Got {val}"),
+        None => println!("No value")
+    }
 }
