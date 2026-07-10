98a61a0372b1bf953bc5c480c1eeddc65ea643ac
SimonNRisk <76711152+SimonNRisk@users.noreply.github.com>
2026-07-10 18:22:44 -0400

    fix: update main.rs using the new lib

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use simonkv::KVStore;
 
 fn main() {
-    let mut kvstore = KVStore::new();
-    kvstore.set(String::from("Test"), String::from("1"));
-    let fetched_val = kvstore.get("Test");
+    let mut kvstore = KVStore::open("simonkv.log").unwrap();
+
+    kvstore.set(String::from("K"), String::from("V")).unwrap();
+
+    let fetched_val = kvstore.get("K");
     match fetched_val {
         Some(val) => println!("Got {val}"),
         None => println!("No value"),
     }
+
     let fetched_fake_val = kvstore.get("Fake");
     match fetched_fake_val {
         Some(val) => println!("Got {val}"),
         None => println!("No value"),
     }
-    let deleted_fetch_val = kvstore.delete("Test");
+
+    let deleted_fetch_val = kvstore.delete("Test").unwrap();
     match deleted_fetch_val {
         Some(val) => println!("Successfully deleted {val}"),
         None => println!("Could not delete"),
