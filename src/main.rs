334ffc9fc26fdb05cbe8de9b075a40eeee6018c3
Simon Risk <simonnrisk@gmail.com>
2026-07-25 18:34:42 -0400

    chore: implement display trait for kvstore, cleanup main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,48 @@
 use simonkv::KVStore;
 
 fn main() {
+    /*
+       Remember when inspecting logs, byte offsets include header and key
+       Header = Operation (1 byte) + Key Length (2 bytes) + Value Length (4 bytes) = 7 bytes
+    */
+
     let mut kvstore = KVStore::open("simonkv.log").unwrap();
 
-    kvstore.set(String::from("K"), String::from("V")).unwrap();
+    let k = String::from("K");
+    let v = String::from("V");
 
-    let fetched_val = kvstore.get("K").unwrap();
+    kvstore.set(k.clone(), v.clone()).unwrap();
+
+    println!("Set {k} to {v} in keydir: {kvstore}");
+
+    let a = String::from("A");
+    let b = String::from("B");
+
+    kvstore.set(a.clone(), b.clone()).unwrap();
+
+    println!("Set {a} to {b} in keydir: {kvstore}");
+
+    drop(kvstore);
+
+    println!("Dropped kvstore");
+
+    let mut kvstore = KVStore::open("simonkv.log").unwrap();
+
+    let fetched_val = kvstore.get(&k).unwrap();
     match fetched_val {
-        Some(val) => println!("Got {val}"),
+        Some(val) => println!("Got {val} for {k}"),
         None => println!("No value"),
     }
 
-    let fetched_fake_val = kvstore.get("Fake").unwrap();
+    let fake = String::from("Fake");
+
+    let fetched_fake_val = kvstore.get(&fake).unwrap();
     match fetched_fake_val {
-        Some(val) => println!("Got {val}"),
-        None => println!("No value"),
+        Some(val) => println!("Got {val}"), // wont run
+        None => println!("No value for {fake} found"),
     }
 
-    let deleted_fetch_val = kvstore.delete("Test").unwrap();
-    match deleted_fetch_val {
-        Some(val) => println!("Successfully deleted {val}"),
-        None => println!("Could not delete"),
-    }
+    println!("Compacting...");
+    kvstore.compact().unwrap();
+    println!("Compacted kvstore: {kvstore}");
 }
